030b4f35ea24d66678faf0795060f4439dfa53c7
Сидоров Александр Валерьевич <sidorovav@gmail.com>
2019-10-15 10:21:33 +0300

    add install

---
+++ AspNetReact/install.cmd
@@ -0,0 +1,2 @@
+npm install --save-dev @babel/cli @babel/core @babel/plugin-proposal-class-properties @babel/preset-env @babel/preset-react babel-loader browser-sync browser-sync-webpack-plugin webpack webpack-cli webpack-notifier
+npm install react react-dom
